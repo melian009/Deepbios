d6414bf99466a8393cea43b1c440e13a2517b121
Ali R. Vahdati <arv.kavir@gmail.com>
2021-03-04 10:29:06 +0100

    Update BN code

--- src/bnlearn.r
+++ src/bnlearn.r
@@ -23,6 +23,41 @@ df[, 1:20] <- lapply(df[,1:20], as.factor)
 wl = read.csv("node_whitelist.csv")
 bl = read.csv("node_blacklist.csv")
 
-## Structure learning with PC algorithm. To use other algorithms, `?inter.iamb
+## Structure learning with PC algorithm. Does not result in any reasonable network
+bn_pc = pc.stable(df, whitelist=wl, blacklist=bl)
+plot(bn_pc)
+# GS algorithm: Error in if (a <= alpha) { : missing value where TRUE/FALSE needed
+bn_gs = gs(df, whitelist=wl, blacklist=bl)
+plot(bn_gs)
+# IAMB algorithm: Error in if (a <= alpha) { : missing value where TRUE/FALSE needed
+bn_iamb = iamb(df, whitelist=wl, blacklist=bl)
+plot(bn_iamb)
+# inter-IAMB algorithm
+bn_interiamb = inter.iamb(df, whitelist=wl, blacklist=bl)
+plot(bn_interiamb)
+# IAMB-FDR algorithm: Error in if (a <= alpha) { : missing value where TRUE/FALSE needed
+bn_iambfdr = iamb.FDR(df, whitelist=wl, blacklist=bl)
+plot(bn_iambfdr)
+# MMPC algorithm
+bn_mmpc = mmpc(df, whitelist=wl, blacklist=bl)
+plot(bn_mmpc)
+# SI.HITON-PC algorithm
+bn_sihitonpc = si.hiton.pc(df, whitelist=wl, blacklist=bl)
+plot(bn_sihitonpc)
+# HC algorithm (score-based): The most reasonable so far
+bn_hc = hc(df, whitelist=wl, blacklist=bl)
+plot(bn_hc)
+bn_hc_fitted = bn.fit(bn_hc, df)
+write.net("bn_hc.net", bn_hc_fitted)
+# # HC algorithm (score-based) with restars
+# bn_hc_restart = hc(df, whitelist=wl, blacklist=bl, restart=100)
+# plot(bn_hc_restart)
+# bn_hc_fitted_restart = bn.fit(bn_hc_restart, df)
+# write.net("bn_hc_restart.net", bn_hc_fitted_restart)
+# MMHC algorithm (hybrid)
+bn_mmhc = mmhc(df, whitelist=wl, blacklist=bl)
+plot(bn_mmhc)
+# RSMAX2 algorithm (hybrid)
+bn_rsmax2 = rsmax2(df, whitelist=wl, blacklist=bl)
+plot(bn_rsmax2)
 
-bn = pc.stable(df, whitelist=wl, blacklist=bl)
