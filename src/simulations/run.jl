ecf24892c3096bc1cc484a46f60302c72d9a4cad
Ali R. Vahdati <arv.kavir@gmail.com>
2021-08-24 19:04:34 +0200

    Create sim params for 12 species

--- src/simulations/run.jl
+++ src/simulations/run.jl
@@ -3,8 +3,16 @@ using Pkg
 Pkg.activate(".")
 using Statistics
 using Agents
+using DataFrames
+using CSV
 include("collection_functions.jl")
+include("init.jl")
 
 param_file = "initialization.yml"
+if !isfile(param_file)
+  data = create_dicts()
+  EvoDynamics.YAML.write_file(param_file, data)
+end
 
-_, results = EvoDynamics.runmodel(param_file, adata=nothing, mdata=[count_per_site, age_per_site, migration_rates_per_site, abiotic_value_per_site], when=[1, 99, 100])
+
+_, results = EvoDynamics.runmodel(param_file, adata=nothing, mdata=[count_per_site, age_per_site, migration_rates_per_site, abiotic_value_per_site], when = [1, 99, 100])
